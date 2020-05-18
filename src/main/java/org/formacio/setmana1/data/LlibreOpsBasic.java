082052180cd3a98947f62ea89cbcf000da0cc59b
DanyZurita <dzurita@cifpfbmoll.eu>
2020-05-18 10:53:43 +0200

    feat(LlibreOpsBasic): Añadir EntityManager y logica de carrega() para dos casos test

--- src/main/java/org/formacio/setmana1/data/LlibreOpsBasic.java
+++ src/main/java/org/formacio/setmana1/data/LlibreOpsBasic.java
@@ -1,6 +1,9 @@
 package org.formacio.setmana1.data;
 
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.formacio.setmana1.domini.Llibre;
 import org.formacio.setmana1.domini.Recomanacio;
 import org.springframework.stereotype.Component;
@@ -12,11 +15,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class LlibreOpsBasic {
 	
+	@PersistenceContext
+	private EntityManager em;
+	
 	/**
 	 * Retorna el llibre amb l'ISBN indicat o, si no existeix, llança un LlibreNoExisteixException
 	 */
 	public Llibre carrega (String isbn) throws LlibreNoExisteixException {
-		return null;
+		Llibre exemplar = em.find(Llibre.class, isbn);
+		if(exemplar != null){ 
+			return exemplar;
+		}
+		else {
+			throw new LlibreNoExisteixException();	
+		}
 	}
 	
 	/**
